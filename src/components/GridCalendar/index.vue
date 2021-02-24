e6d465aba1103659584793fbda0d5ce2a0239264
oliseulean <43409070+oliseulean@users.noreply.github.com>
2021-02-24 21:45:01 +0200

    Updated GridCalendar.vue
    Removed commented code

--- src/components/GridCalendar/index.vue
+++ src/components/GridCalendar/index.vue
@@ -1,10 +1,7 @@
 <template>
     <div :class="contentClass">
       <div :class="weekClass" v-for="(week,index) in weeks" :key="index">
-        <!-- TODO change the div for weekClass ????-->
         <CalendarDay v-for="(day,index) in week" :key="index" :day="day"/>
-        <!-- TODO sa fac formatarea in CalendarDay !fixed-->
-        <!-- TODO sa verific aceeasi data daca egala cu cea de astazi !fixed-->
       </div>
     </div>
 </template>
